6da8d6b69a0517b834f65c8ce14c064dbf37b2aa
Andrei POPOVICIU <apopoviciu@bitdefender.biz>
2023-11-26 21:45:34 +0200

    added support for loading the data in HF format

--- dataloaders/iemocap.py
+++ dataloaders/iemocap.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 import torch.nn.functional
+from datasets import Dataset as HFDataset
 
 from torch.utils.data import Dataset, DataLoader
 
@@ -61,3 +62,41 @@ def IemocapDataLoader(
     return DataLoader(
         IemocapDataset(dataset_path, audio_and_text_csv, emotions, division), **kwargs
     )
+
+
+class IemocapHFDataset:
+    def __init__(
+            self,
+            dataset_path: str,
+            audio_and_text_csv: str,
+            emotions: list[str],
+            division: str):
+        self._dataset_path = dataset_path
+        self._emotions = np.array(emotions)
+        dataframe = pd.read_csv(audio_and_text_csv)
+        dataframe = dataframe[dataframe["emotion"].isin(emotions)][::24]
+        rows_80_percent = int(0.8 * len(dataframe))
+        if division == "train":
+            dataframe = dataframe.iloc[:rows_80_percent, :]
+        elif division == "test":
+            dataframe = dataframe.iloc[rows_80_percent:, :]
+        else:
+            raise ValueError("Invalid dataset division")
+
+        self._data = self._preprocess_data(dataframe)
+
+        self.dataset = HFDataset.from_pandas(pd.DataFrame(self._data))
+        
+        self.dataset = self.dataset.rename_column("emotion_index", "labels")
+
+    def _preprocess_data(self, dataframe):
+        data = []
+        for _, row in dataframe.iterrows():
+            _, text, emotion = row
+            emotion_index = np.where(self._emotions == emotion)[0][0]
+            data.append({'text': text,
+                        'emotion_index': emotion_index})
+        return data
+
+    def get_hf_dataset(self):
+        return self.dataset
